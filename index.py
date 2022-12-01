6ec2d8ea2d2835366bfc7c6f5b8b1bcc946244a9
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-12-01 04:42:20 +0100

    Add commands

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 import sys
+import json
 
 from jinja2_static import Builder
 
 build = Builder()
 argument = sys.argv[1] if len(sys.argv) > 1 else None
 
+with open("./static/data/commands.json", "r", encoding="utf8") as f:
+    commands = json.load(f)
+
 build.generate(
     debug=False if argument == "build" else True,
-    URL="https://kawaiibot.dev"
+    URL="https://kawaiibot.dev", commands=commands
 )
